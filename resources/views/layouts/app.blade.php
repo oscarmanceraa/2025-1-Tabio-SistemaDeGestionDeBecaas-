619f94f50acdb77dae296e7635a4373e9c03fcc4
oscarmanceraa <oscarmancera20@gmail.com>
2025-05-06 23:37:19 -0500

    cambios esteticos login y register

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,8 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>SGBTABIO</title>
+    <link rel="icon" type="image/png" href="{{ asset('images/logo.png') }}">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="{{asset('assets/login.css')}}">
+    <link rel="stylesheet" href="{{ asset('css/login.css') }}">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
